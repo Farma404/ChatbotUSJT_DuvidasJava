998ba8a963b1ed68f62840e6bf31c0a74fc93830
Weslley Felicio Batista <weslley_b2008@outlook.com>
2021-05-17 10:07:13 -0300

    Atualizando Link

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ bot.on("callback_query", function (query) {
     }
     else{
         queries[query.id] = query;
-        let gameurl = "http://127.0.0.1:5500/jogo/index.html?id=" + query.id;
+        let gameurl = "https://git.heroku.com/prog-quiz-game/index.html?id=" + query.id;
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url:gameurl
